7016059fab0de2dc8034d5a68fc54525d5c61612
Zheyuan Chen <sephirotheca17@gmail.com>
2025-10-01 23:09:22 -0700

    Limit simt-opt dependencies to required dialects

--- tools/simt-opt/src/main.cpp
+++ tools/simt-opt/src/main.cpp
@@ -1,9 +1,18 @@
 #include "simt-step/Conversion/SimtStepToStructured.h"
 #include "simt-step/Dialect/SimtStep/SimtStepDialect.h"
 #include "simt-step/Dialect/SimtStructured/StructuredDialect.h"
+#include "simt-step/semantics/StructuredProgram.h"
 
-#include <mlir/InitAllDialects.h>
-#include <mlir/InitAllPasses.h>
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/ControlFlow/IR/ControlFlow.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
+#include <mlir/Dialect/MemRef/IR/MemRef.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
+#include <mlir/Dialect/Tensor/IR/Tensor.h>
+#include <mlir/Dialect/Vector/IR/VectorOps.h>
+#include <mlir/IR/BuiltinDialect.h>
+#include <mlir/IR/DialectRegistry.h>
 #include <mlir/Support/LogicalResult.h>
 #include <mlir/Tools/mlir-opt/MlirOptMain.h>
 
@@ -11,12 +20,16 @@ using namespace mlir;
 
 int main(int argc, char **argv) {
   DialectRegistry registry;
-  registerAllDialects(registry);
+  registry.insert<mlir::BuiltinDialect, mlir::arith::ArithDialect,
+                  mlir::cf::ControlFlowDialect, mlir::func::FuncDialect,
+                  mlir::LLVM::LLVMDialect, mlir::memref::MemRefDialect,
+                  mlir::scf::SCFDialect, mlir::tensor::TensorDialect,
+                  mlir::vector::VectorDialect>();
   simt::dialect::registerSimtStepDialect(registry);
   simt::structured::registerSimtStructuredDialect(registry);
 
-  registerAllPasses();
   simt::conversion::registerSimtStepToStructuredPass();
+  simt::semantics::registerDumpStructuredProgramPass();
 
   return failed(mlir::MlirOptMain(argc, argv, "SIMT-Step optimizer", registry));
 }
